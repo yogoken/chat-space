8f53562c4aefae5b7d62648b9ab07e3f7ff1f963
yogoken <yogo.k@husky.neu.edu>
2016-10-09 09:19:32 +0900

    Add create action and display flash message in application view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,20 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = Message.all
   end
+
+  def create
+    @message = Message.new(message_params)
+    @messages = Message.all
+    if @message.save
+      redirect_to messages_path, notice: 'return message successfully'
+      flash.now[:alert] = @message.errors.full_messages.join(',' )
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
