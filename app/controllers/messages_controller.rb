6e4d578eaceea3cde1a4a7212ce6e9813a1dc11e
yogoken <yogo.k@husky.neu.edu>
2016-10-10 19:39:11 +0900

    Add @chat_group in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,19 @@
 class MessagesController < ApplicationController
+  before_action :set_chat_groups
+  before_action :set_chat_group
+
   def index
     @message = Message.new
-    @messages = Message.all
+    @messages = @chat_group.messages
   end
 
   def create
     @message = Message.new(message_params)
     @message.user = current_user
-    @messages = Message.all
+    @message.chat_group = @chat_group
+
     if @message.save
-      redirect_to messages_path, notice: 'Successfully posted'
+      redirect_to chat_group_messages_path(@chat_group), notice: 'メッセージを送信しました'
     else
       flash.now[:alert] = @message.errors.full_messages.join(', ')
       render :index
@@ -17,6 +21,14 @@ class MessagesController < ApplicationController
   end
 
   private
+
+  def set_chat_groups
+    @chat_groups = ChatGroup.all
+  end
+
+  def set_chat_group
+    @chat_group = ChatGroup.find(params[:chat_group_id])
+  end
 
   def message_params
     params.require(:message).permit(:body)
