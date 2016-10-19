79de736d74cad67ba4a507f4831e5a835037fbed
yogoken <yogo.k@husky.neu.edu>
2016-10-19 09:56:40 +0900

    Add edit_chat_group.js and refactoring codes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE ?', "%#{params[:name]}%")
+    respond_to do |format|
+      format.json { render json: @users }
+    end
+  end
+end
