d0a1b52f84f59db357cc5a29b212f60d4cbb18e7
yogoken <yogo.k@husky.neu.edu>
2016-10-31 19:18:02 +0900

    Add ImagesUploader in model

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,11 +6,15 @@ class Message < ApplicationRecord
   validates :body, presence: true
   validates :user, presence: true
 
+  # 画像投稿
+  mount_uploader :image, ImagesUploader
+
   def to_api_json
     {
       name: user.name,
       body: body,
-      time: created_at.strftime('%Y/%m/%d %H:%M:%S')
+      time: created_at.strftime('%Y/%m/%d %H:%M:%S'),
+      image_url: image.to_s
     }
   end
 end
